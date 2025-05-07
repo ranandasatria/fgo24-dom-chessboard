acf3400b31c1f2191b4cf01ce2d5a45f1c3b19cf
ranandasatria <rananda24@gmail.com>
2025-05-07 16:44:21 +0700

    chore: add readme and background

---
+++ README.md
@@ -0,0 +1,48 @@
+# Chessboard Layout with Javascript DOM
+
+This is a lightweight JavaScript application that dynamically generates an 8x8 chessboard using DOM manipulation. This project showcases the use of nested for loops to create a grid of alternating black and white squares, styled with CSS and organized within a centered container. 
+
+## Preview
+![Preview](src/preview.png)
+
+
+## Getting Started
+1. Clone this project:
+```
+git clone https://github.com/ranandasatria/fgo24-dom-traffic-light
+```
+
+2. Install the depedencies:
+```
+npm install
+```
+
+3. Run the project:
+```
+npm run dev
+```
+
+4. The project will be runnning at:
+``` 
+http://localhost:8080
+```
+
+## Depedencies
+
+This project uses Node.js. Make sure you have Node.js installed on your machine.
+
+- live-server: to simulate an HTTP server in a local environment.
+
+## How to contribute
+
+Please open a Pull Request (PR) to contribute to this project.
+Your PR will be reviewed and merged if necessary.
+
+## License
+
+This project following MIT License.
+
+## Copyright
+&copy; 2025 Kodacademy
+
+
--- script.js
+++ script.js
@@ -21,3 +21,36 @@ container.appendChild(chessboard)
 
 document.body.appendChild(container)
 
+
+
+//  // Membuat array untuk baris dan kolom (0 sampai 7)
+//  const rows = Array.from({ length: 8 }, (_, i) => i);
+//  const cols = Array.from({ length: 8 }, (_, j) => j);
+
+//  // Membuat chessboard
+//  const chessboard = document.createElement('div');
+//  chessboard.classList.add('chessboard');
+
+//  // Menggunakan forEach untuk membuat kotak
+//  rows.forEach(i => {
+//      cols.forEach(j => {
+//          const square = document.createElement('div');
+//          square.classList.add('square');
+//          if ((i + j) % 2 === 0) {
+//              square.classList.add('white');
+//          } else {
+//              square.classList.add('black');
+//          }
+//          chessboard.appendChild(square);
+//      });
+//  });
+
+//  // Membuat container
+//  const container = document.createElement('div');
+//  container.classList.add('container');
+
+//  // Menambahkan chessboard sebagai child dari container
+//  container.appendChild(chessboard);
+
+//  // Menambahkan container ke body
+//  document.body.appendChild(container);
